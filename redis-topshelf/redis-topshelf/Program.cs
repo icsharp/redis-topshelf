f86c736fc79c3668a557783bf3ae605d350ac758
vigoss <xuxi.csharp@gmail.com>
2014-07-02 20:57:36 +0800

    create the project

---
+++ redis-topshelf/redis-topshelf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace redis_topshelf
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
